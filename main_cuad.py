ca419a3da09918a406eca1a11152c35486ed4d5c
lucia <curtolucia@gmail.com>
2024-04-16 15:36:45 -0300

    ma16abr cambio de fecha

--- main_cuad.py
+++ main_cuad.py
@@ -12,14 +12,14 @@ from func_wc_S import flujo_wc_S
 # from flujo_wT_cuad import flujo_wT_cuad
 # from flujo_wT_S_H import flujo_wT_S_H
 
-ruta_entrada_faltantes = '.././faltantes/2021-2022/SM_140/2021/03/'
-ruta_entrada_calidad = '.././calidad/2021-2022/SM_140/2021/03/'
+ruta_entrada_faltantes = '.././faltantes/2021-2022/SM_140/2022/08/'
+ruta_entrada_calidad = '.././calidad/2021-2022/SM_140/2022/08/'
 
-ruta_entrada_desvios = '.././desvios/2021-2022/SM_140/2021/03/'
-ruta_entrada_medios = '.././medios/2021-2022/SM_140/2021/03/'
+ruta_entrada_desvios = '.././desvios/2021-2022/SM_140/2022/08/'
+ruta_entrada_medios = '.././medios/2021-2022/SM_140/2022/08/'
 
-ruta_salida_cuad_uw = '.././cuadrantes/flujo_uw/2021-2022/SM_140/2021/03/'
-ruta_salida_S_uw = '.././SiH/Si0/flujo_uw/2021-2022/SM_140/2021/03/'
+ruta_salida_cuad_uw = '.././cuadrantes/flujo_uw/2021-2022/SM_140/2022/08/'
+ruta_salida_S_uw = '.././SiH/Si0/flujo_uw/2021-2022/SM_140/2022/08/'
 
 #Creo el directorio de salida (ruta_salida) si no existe
 if not os.path.exists(ruta_salida_cuad_uw):
@@ -28,8 +28,8 @@ if not os.path.exists(ruta_salida_cuad_uw):
 if not os.path.exists(ruta_salida_S_uw):
     os.makedirs(ruta_salida_S_uw)
 
-ruta_salida_cuad_wT = '.././cuadrantes/flujo_wT/2021-2022/SM_140/2021/03/'
-ruta_salida_S_wT = '.././SiH/Si0/flujo_wT/2021-2022/SM_140/2021/03/'
+ruta_salida_cuad_wT = '.././cuadrantes/flujo_wT/2021-2022/SM_140/2022/08/'
+ruta_salida_S_wT = '.././SiH/Si0/flujo_wT/2021-2022/SM_140/2022/08/'
 
 #Creo el directorio de salida (ruta_salida) si no existe
 if not os.path.exists(ruta_salida_cuad_wT):
@@ -38,8 +38,8 @@ if not os.path.exists(ruta_salida_cuad_wT):
 if not os.path.exists(ruta_salida_S_wT):
     os.makedirs(ruta_salida_S_wT)
 
-ruta_salida_cuad_wc = '.././cuadrantes/flujo_wc/2021-2022/SM_140/2021/03/'
-ruta_salida_S_wc = '.././SiH/Si0/flujo_wc/2021-2022/SM_140/2021/03/'
+ruta_salida_cuad_wc = '.././cuadrantes/flujo_wc/2021-2022/SM_140/2022/08/'
+ruta_salida_S_wc = '.././SiH/Si0/flujo_wc/2021-2022/SM_140/2022/08/'
 
 #Creo el directorio de salida (ruta_salida) si no existe
 if not os.path.exists(ruta_salida_cuad_wc):
@@ -48,21 +48,21 @@ if not os.path.exists(ruta_salida_cuad_wc):
 if not os.path.exists(ruta_salida_S_wc):
     os.makedirs(ruta_salida_S_wc)
 
-datos = 'SM_140_2021-03-'
+datos = 'SM_140_2022-08-'
 
-for day in range(31, 32):
-    for arch in range(1430, 1500):
-        if not os.path.isfile(ruta_entrada_desvios + datos + str(day) + '_' + str(arch).zfill(4) + '.csv'):
+for day in range(1, 2):
+    for arch in range(30, 2400):
+        if not os.path.isfile(ruta_entrada_medios + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv'):
             continue
         else:
-            flujos_desvios = pd.read_csv(ruta_entrada_desvios + datos + str(day) + '_' + str(arch).zfill(4) + \
+            flujos_desvios = pd.read_csv(ruta_entrada_desvios + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + \
             '.csv',header=0,na_values='NAN') #lee el archivo guardado
-            flujos_medios = pd.read_csv(ruta_entrada_medios + datos + str(day) + '_' + str(arch).zfill(4) + \
+            flujos_medios = pd.read_csv(ruta_entrada_medios + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + \
             '.csv',header=0,na_values='NAN') #lee el archivo guardado
 
-            calidad = pd.read_csv(ruta_entrada_calidad + datos + str(day) + '_' + str(arch).zfill(4) + \
+            calidad = pd.read_csv(ruta_entrada_calidad + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + \
             '.csv',header=0,na_values='NAN')
-            faltantes = pd.read_csv(ruta_entrada_faltantes + datos + str(day) + '_' + str(arch).zfill(4) + \
+            faltantes = pd.read_csv(ruta_entrada_faltantes + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + \
             '.csv',header=0,na_values='NAN')
 
             lim_calidad = 250
@@ -77,6 +77,7 @@ for day in range(31, 32):
             col_uw = ['u_1.5m', 'w_1.5m', 'u_3m', 'w_3m', 'u_5m', 'w_5m', \
             'u_7m', 'w_7m']
             check_faltantes_uw = (faltantes[col_uw] < lim_faltantes).all(axis=1)
+            print(check_faltantes_uw)
 
 #-------------------------------------------------------------------------------------------
 # metodo de los cuadrantes para uw
@@ -85,10 +86,10 @@ for day in range(31, 32):
             if check_calidad_uw.all() & check_faltantes_uw.all():
 
                 totales_uw_cuad = flujo_uw_cuad(flujos_desvios)
-                totales_uw_cuad.to_csv (ruta_salida_cuad_uw + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                totales_uw_cuad.to_csv (ruta_salida_cuad_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
                 S_uw_cuad = flujo_uw_S(totales_uw_cuad,flujos_medios)
-                S_uw_cuad.to_csv (ruta_salida_S_uw + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                S_uw_cuad.to_csv (ruta_salida_S_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
             else:
                 continue
@@ -110,10 +111,10 @@ for day in range(31, 32):
             if check_calidad_wT.all() & check_faltantes_wT.all():
 
                 totales_wT_cuad = flujo_wT_cuad(flujos_desvios)
-                totales_wT_cuad.to_csv (ruta_salida_cuad_wT + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                totales_wT_cuad.to_csv (ruta_salida_cuad_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
                 S_wT_cuad = flujo_wT_S(totales_wT_cuad,flujos_medios)
-                S_wT_cuad.to_csv (ruta_salida_S_wT + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                S_wT_cuad.to_csv (ruta_salida_S_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
             else:
                 continue
@@ -134,10 +135,10 @@ for day in range(31, 32):
             if check_calidad_wc.all() & check_faltantes_wc.all():
 
                 totales_wc_cuad = flujo_wc_cuad(flujos_desvios)
-                totales_wc_cuad.to_csv (ruta_salida_cuad_wc + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                totales_wc_cuad.to_csv (ruta_salida_cuad_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
                 S_wc_cuad = flujo_wc_S(totales_wc_cuad,flujos_medios)
-                S_wc_cuad.to_csv (ruta_salida_S_wc + datos + str(day) + '_' + str(arch) + '.csv', index=False)
+                S_wc_cuad.to_csv (ruta_salida_S_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
 
             else:
                 continue
