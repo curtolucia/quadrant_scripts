efee894d349b56ddbc9aaa2c7433110ec116b7a4
curtolucia <curtolucia@gmail.com>
2024-04-22 23:01:54 -0300

    lu22abr prueba cambios mejora niveles

--- func_uw_cuad.py
+++ func_uw_cuad.py
@@ -13,72 +13,92 @@ def flujo_uw_cuad(flujos_desvios):
     flujos_desvios = flujos_desvios.copy()
 
 #leo de los archivos las columnas correspondientes a u
-    u_desvio_1_5 = flujos_desvios['u_desvio_1.5m']
-    u_desvio_3 = flujos_desvios['u_desvio_3m']
-    u_desvio_5 = flujos_desvios['u_desvio_5m']
-    u_desvio_7 = flujos_desvios['u_desvio_7m']
-
+    u_desvio = flujos_desvios['u_desvio']
 #leo de los archivos las columnas correspondientes a v
-    v_desvio_1_5 = flujos_desvios['v_desvio_1.5m']
-    v_desvio_3 = flujos_desvios['v_desvio_3m']
-    v_desvio_5 = flujos_desvios['v_desvio_5m']
-    v_desvio_7 = flujos_desvios['v_desvio_7m']
-
+    v_desvio = flujos_desvios['v_desvio']
 #leo de los archivos las columnas correspondientes a w
-    w_desvio_1_5 = flujos_desvios['w_desvio_1.5m']
-    w_desvio_3 = flujos_desvios['w_desvio_3m']
-    w_desvio_5 = flujos_desvios['w_desvio_5m']
-    w_desvio_7 = flujos_desvios['w_desvio_7m']
-
-#leo las cov de cantidad de movimiento uw y vw
-#para los niveles de 1.5 m, 3 m, 5 m y 7 m
-    cov_uw_1_5 = flujos_desvios['cov_uw_1.5m']
-    cov_uw_3 = flujos_desvios['cov_uw_3m']
-    cov_uw_5 = flujos_desvios['cov_uw_5m']
-    cov_uw_7 = flujos_desvios['cov_uw_7m']
-
-    cov_vw_1_5 = flujos_desvios['cov_vw_1.5m']
-    cov_vw_3 = flujos_desvios['cov_vw_3m']
-    cov_vw_5 = flujos_desvios['cov_vw_5m']
-    cov_vw_7 = flujos_desvios['cov_vw_7m']
+    w_desvio = flujos_desvios['w_desvio']
+# #leo las cov de cantidad de movimiento uw y vw
+    cov_uw = flujos_desvios['cov_uw']
+    cov_vw = flujos_desvios['cov_vw']
 
 # Creo lista para almacenar los datos
     cuad_inst = []
-    cuad_inst_1_5 = []
-    cuad_inst_3 = []
-    cuad_inst_5 = []
-    cuad_inst_7 = []
-
 # Iterar para todos los valores instantaneos de uw
 #para los niveles de 1.5 m, 3 m, 5 m y 7 m
-    for u_d, w_d, cov_d in zip(u_desvio_1_5, w_desvio_1_5, cov_uw_1_5):
+    for u_d, w_d, cov_d in zip(u_desvio, w_desvio, cov_uw):
         cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
 # Guardar los resultados en la variable 'cuad_inst_1_5'
-        cuad_inst_1_5.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
-    cuad_inst_1_5_df = pd.DataFrame(cuad_inst_1_5, columns=['u_desvio_1.5m', 'w_desvio_1.5m', \
-    'cov_uw_c1_1.5m', 'cov_uw_c2_1.5m', 'cov_uw_c3_1.5m', 'cov_uw_c4_1.5m'])
+        cuad_inst.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
 
 
-    for u_d, w_d, cov_d in zip(u_desvio_3, w_desvio_3, cov_uw_3):
-        cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
-# Guardar los resultados en la variable 'cuad_inst_3'
-        cuad_inst_3.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
-    cuad_inst_3_df = pd.DataFrame(cuad_inst_3, columns=['u_desvio_3m', 'w_desvio_3m', \
-    'cov_uw_c1_3m', 'cov_uw_c2_3m', 'cov_uw_c3_3m', 'cov_uw_c4_3m'])
-
-    for u_d, w_d, cov_d in zip(u_desvio_5, w_desvio_5, cov_uw_5):
-        cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
-# Guardar los resultados en la variable 'cuad_inst_5'
-        cuad_inst_5.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
-    cuad_inst_5_df = pd.DataFrame(cuad_inst_5, columns=['u_desvio_5m', 'w_desvio_5m', \
-    'cov_uw_c1_5m', 'cov_uw_c2_5m', 'cov_uw_c3_5m', 'cov_uw_c4_5m'])
-
-    for u_d, w_d, cov_d in zip(u_desvio_7, w_desvio_7, cov_uw_7):
-        cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
-# Guardar los resultados en la variable 'cuad_inst_7'
-        cuad_inst_7.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
-    cuad_inst_7_df = pd.DataFrame(cuad_inst_7, columns=['u_desvio_7m', 'w_desvio_7m', \
-    'cov_uw_c1_7m', 'cov_uw_c2_7m', 'cov_uw_c3_7m', 'cov_uw_c4_7m'])
-
-    cuad_inst_df = pd.concat([cuad_inst_1_5_df,cuad_inst_3_df,cuad_inst_5_df,cuad_inst_7_df], axis=1)
-    return cuad_inst_df
+# #leo de los archivos las columnas correspondientes a u
+#     u_desvio_1_5 = flujos_desvios['u_desvio_1.5m']
+#     u_desvio_3 = flujos_desvios['u_desvio_3m']
+#     u_desvio_5 = flujos_desvios['u_desvio_5m']
+#     u_desvio_7 = flujos_desvios['u_desvio_7m']
+#
+# #leo de los archivos las columnas correspondientes a v
+#     v_desvio_1_5 = flujos_desvios['v_desvio_1.5m']
+#     v_desvio_3 = flujos_desvios['v_desvio_3m']
+#     v_desvio_5 = flujos_desvios['v_desvio_5m']
+#     v_desvio_7 = flujos_desvios['v_desvio_7m']
+#
+# #leo de los archivos las columnas correspondientes a w
+#     w_desvio_1_5 = flujos_desvios['w_desvio_1.5m']
+#     w_desvio_3 = flujos_desvios['w_desvio_3m']
+#     w_desvio_5 = flujos_desvios['w_desvio_5m']
+#     w_desvio_7 = flujos_desvios['w_desvio_7m']
+#
+# #leo las cov de cantidad de movimiento uw y vw
+# #para los niveles de 1.5 m, 3 m, 5 m y 7 m
+#     cov_uw_1_5 = flujos_desvios['cov_uw_1.5m']
+#     cov_uw_3 = flujos_desvios['cov_uw_3m']
+#     cov_uw_5 = flujos_desvios['cov_uw_5m']
+#     cov_uw_7 = flujos_desvios['cov_uw_7m']
+#
+#     cov_vw_1_5 = flujos_desvios['cov_vw_1.5m']
+#     cov_vw_3 = flujos_desvios['cov_vw_3m']
+#     cov_vw_5 = flujos_desvios['cov_vw_5m']
+#     cov_vw_7 = flujos_desvios['cov_vw_7m']
+#
+# # Creo lista para almacenar los datos
+#     cuad_inst = []
+#     cuad_inst_1_5 = []
+#     cuad_inst_3 = []
+#     cuad_inst_5 = []
+#     cuad_inst_7 = []
+#
+# # Iterar para todos los valores instantaneos de uw
+# #para los niveles de 1.5 m, 3 m, 5 m y 7 m
+#     for u_d, w_d, cov_d in zip(u_desvio_1_5, w_desvio_1_5, cov_uw_1_5):
+#         cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
+# # Guardar los resultados en la variable 'cuad_inst_1_5'
+#         cuad_inst_1_5.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
+#     cuad_inst_1_5_df = pd.DataFrame(cuad_inst_1_5, columns=['u_desvio_1.5m', 'w_desvio_1.5m', \
+#     'cov_uw_c1_1.5m', 'cov_uw_c2_1.5m', 'cov_uw_c3_1.5m', 'cov_uw_c4_1.5m'])
+#
+#
+#     for u_d, w_d, cov_d in zip(u_desvio_3, w_desvio_3, cov_uw_3):
+#         cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
+# # Guardar los resultados en la variable 'cuad_inst_3'
+#         cuad_inst_3.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
+#     cuad_inst_3_df = pd.DataFrame(cuad_inst_3, columns=['u_desvio_3m', 'w_desvio_3m', \
+#     'cov_uw_c1_3m', 'cov_uw_c2_3m', 'cov_uw_c3_3m', 'cov_uw_c4_3m'])
+#
+#     for u_d, w_d, cov_d in zip(u_desvio_5, w_desvio_5, cov_uw_5):
+#         cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
+# # Guardar los resultados en la variable 'cuad_inst_5'
+#         cuad_inst_5.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
+#     cuad_inst_5_df = pd.DataFrame(cuad_inst_5, columns=['u_desvio_5m', 'w_desvio_5m', \
+#     'cov_uw_c1_5m', 'cov_uw_c2_5m', 'cov_uw_c3_5m', 'cov_uw_c4_5m'])
+#
+#     for u_d, w_d, cov_d in zip(u_desvio_7, w_desvio_7, cov_uw_7):
+#         cov_uw_c1, cov_uw_c2, cov_uw_c3, cov_uw_c4 = calc_cuad(u_d, w_d, cov_d)
+# # Guardar los resultados en la variable 'cuad_inst_7'
+#         cuad_inst_7.append([u_d, w_d, cov_uw_c1.tolist(), cov_uw_c2.tolist(), cov_uw_c3.tolist(), cov_uw_c4.tolist()])
+#     cuad_inst_7_df = pd.DataFrame(cuad_inst_7, columns=['u_desvio_7m', 'w_desvio_7m', \
+#     'cov_uw_c1_7m', 'cov_uw_c2_7m', 'cov_uw_c3_7m', 'cov_uw_c4_7m'])
+#
+#     cuad_inst_df = pd.concat([cuad_inst_1_5_df,cuad_inst_3_df,cuad_inst_5_df,cuad_inst_7_df], axis=1)
+#     return cuad_inst_df
--- main_cuad.py
+++ main_cuad.py
@@ -68,77 +68,97 @@ for day in range(1, 2):
             lim_calidad = 250
             lim_faltantes = 30
 
-#-------------------------------------------------------------------------------------------
-#chequeo de calidad de datos y faltantes para uw
-#-------------------------------------------------------------------------------------------
-            col_cov_uw = ['cov_uw_1.5m', 'cov_uw_3m', 'cov_uw_5m', 'cov_uw_7m']
-            check_calidad_uw = (calidad[col_cov_uw] < lim_calidad).all(axis=1)
+            col_cov_uw_1_5 = ['cov_uw_1.5m']
+            check_calidad_uw_1_5 = (calidad[col_cov_uw_1_5] < lim_calidad).all(axis=1)
 
-            col_uw = ['u_1.5m', 'w_1.5m', 'u_3m', 'w_3m', 'u_5m', 'w_5m', \
-            'u_7m', 'w_7m']
-            check_faltantes_uw = (faltantes[col_uw] < lim_faltantes).all(axis=1)
-            print(check_faltantes_uw)
+            col_uw_1_5 = ['u_1.5m', 'w_1.5m']
+            check_faltantes_uw_1_5 = (faltantes[col_uw_1_5] < lim_faltantes).all(axis=1)
 
-#-------------------------------------------------------------------------------------------
-# metodo de los cuadrantes para uw
-#-------------------------------------------------------------------------------------------
-#para los niveles de 1.5 m, 3 m, 5 m y 7 m
-            if check_calidad_uw.all() & check_faltantes_uw.all():
+            flujos_desvio_1_5 = pd.DataFrame(flujos_desvios['u_desvio_1.5m'], flujos_desvios['v_desvio_1.5m'], \
+            flujos_desvios['w_desvio_1.5m'], \
+            flujos_desvios['cov_uw_1.5m'], cov_vw_1_5 = flujos_desvios['cov_vw_1.5m'], \
+            index=['u_desvio','v_desvio','w_desvio','cov_uw','cov_vw'])
 
-                totales_uw_cuad = flujo_uw_cuad(flujos_desvios)
-                totales_uw_cuad.to_csv (ruta_salida_cuad_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
-
-                S_uw_cuad = flujo_uw_S(totales_uw_cuad,flujos_medios)
-                S_uw_cuad.to_csv (ruta_salida_S_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+            if check_calidad_uw_1_5.all() & check_calidad_uw_1_5.all():
+                totales_uw_cuad_1_5 = flujo_uw_cuad(flujos_desvio_1_5)
+                S_uw_cuad_1_5 = flujo_uw_S(totales_uw_cuad_1_5,flujos_medios_1_5)
 
             else:
-                continue
+                totales_uw_cuad_1_5 = np.nan
+                S_uw_cuad_1_5 = np.nan
 
-#-------------------------------------------------------------------------------------------
-#chequeo de calidad de datos y faltantes para wT
-#-------------------------------------------------------------------------------------------
-            col_cov_wT = ['cov_wT_1.5m', 'cov_wT_3m', 'cov_wT_5m', 'cov_wT_7m']
-            check_calidad_wT = (calidad[col_cov_wT] < lim_calidad).all(axis=1)
 
-            col_wT = ['T_1.5m', 'w_1.5m', 'T_3m', 'w_3m', 'T_5m', 'w_5m', \
-            'T_7m', 'w_7m']
-            check_faltantes_wT = (faltantes[col_wT] < lim_faltantes).all(axis=1)
-
-#-------------------------------------------------------------------------------------------
-# metodo de los cuadrantes para wT
-#-------------------------------------------------------------------------------------------
-#para los niveles de 1.5 m, 3 m, 5 m y 7 m
-            if check_calidad_wT.all() & check_faltantes_wT.all():
-
-                totales_wT_cuad = flujo_wT_cuad(flujos_desvios)
-                totales_wT_cuad.to_csv (ruta_salida_cuad_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
-
-                S_wT_cuad = flujo_wT_S(totales_wT_cuad,flujos_medios)
-                S_wT_cuad.to_csv (ruta_salida_S_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
-
-            else:
-                continue
-
-#-------------------------------------------------------------------------------------------
-#chequeo de calidad de datos y faltantes para wc (wCO2 y wq)
-#-------------------------------------------------------------------------------------------
-            col_cov_wc = ['cov_wT_3m', 'cov_wCO2_3m', 'cov_wq_3m']
-            check_calidad_wc = (calidad[col_cov_wc] < lim_calidad).all(axis=1)
-
-            col_wc = ['T_3m', 'w_3m', 'CO2_3m', 'H2O_3m']
-            check_faltantes_wc = (faltantes[col_wc] < lim_faltantes).all(axis=1)
-
-#-------------------------------------------------------------------------------------------
-# metodo de los cuadrantes para wc (wCO2 y wq)
-#-------------------------------------------------------------------------------------------
-# para el nivel de 3 m
-            if check_calidad_wc.all() & check_faltantes_wc.all():
-
-                totales_wc_cuad = flujo_wc_cuad(flujos_desvios)
-                totales_wc_cuad.to_csv (ruta_salida_cuad_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
-
-                S_wc_cuad = flujo_wc_S(totales_wc_cuad,flujos_medios)
-                S_wc_cuad.to_csv (ruta_salida_S_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
-
-            else:
-                continue
+# #-------------------------------------------------------------------------------------------
+# #chequeo de calidad de datos y faltantes para uw
+# #-------------------------------------------------------------------------------------------
+#             col_cov_uw = ['cov_uw_1.5m', 'cov_uw_3m', 'cov_uw_5m', 'cov_uw_7m']
+#             check_calidad_uw = (calidad[col_cov_uw] < lim_calidad).all(axis=1)
+#
+#             col_uw = ['u_1.5m', 'w_1.5m', 'u_3m', 'w_3m', 'u_5m', 'w_5m', \
+#             'u_7m', 'w_7m']
+#             check_faltantes_uw = (faltantes[col_uw] < lim_faltantes).all(axis=1)
+#
+#
+# #-------------------------------------------------------------------------------------------
+# # metodo de los cuadrantes para uw
+# #-------------------------------------------------------------------------------------------
+# #para los niveles de 1.5 m, 3 m, 5 m y 7 m
+#             if check_calidad_uw.all() & check_faltantes_uw.all():
+#
+#                 totales_uw_cuad = flujo_uw_cuad(flujos_desvios)
+#                 totales_uw_cuad.to_csv (ruta_salida_cuad_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#                 S_uw_cuad = flujo_uw_S(totales_uw_cuad,flujos_medios)
+#                 S_uw_cuad.to_csv (ruta_salida_S_uw + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#             else:
+#                 continue
+#
+# #-------------------------------------------------------------------------------------------
+# #chequeo de calidad de datos y faltantes para wT
+# #-------------------------------------------------------------------------------------------
+#             col_cov_wT = ['cov_wT_1.5m', 'cov_wT_3m', 'cov_wT_5m', 'cov_wT_7m']
+#             check_calidad_wT = (calidad[col_cov_wT] < lim_calidad).all(axis=1)
+#
+#             col_wT = ['T_1.5m', 'w_1.5m', 'T_3m', 'w_3m', 'T_5m', 'w_5m', \
+#             'T_7m', 'w_7m']
+#             check_faltantes_wT = (faltantes[col_wT] < lim_faltantes).all(axis=1)
+#
+# #-------------------------------------------------------------------------------------------
+# # metodo de los cuadrantes para wT
+# #-------------------------------------------------------------------------------------------
+# #para los niveles de 1.5 m, 3 m, 5 m y 7 m
+#             if check_calidad_wT.all() & check_faltantes_wT.all():
+#
+#                 totales_wT_cuad = flujo_wT_cuad(flujos_desvios)
+#                 totales_wT_cuad.to_csv (ruta_salida_cuad_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#                 S_wT_cuad = flujo_wT_S(totales_wT_cuad,flujos_medios)
+#                 S_wT_cuad.to_csv (ruta_salida_S_wT + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#             else:
+#                 continue
+#
+# #-------------------------------------------------------------------------------------------
+# #chequeo de calidad de datos y faltantes para wc (wCO2 y wq)
+# #-------------------------------------------------------------------------------------------
+#             col_cov_wc = ['cov_wT_3m', 'cov_wCO2_3m', 'cov_wq_3m']
+#             check_calidad_wc = (calidad[col_cov_wc] < lim_calidad).all(axis=1)
+#
+#             col_wc = ['T_3m', 'w_3m', 'CO2_3m', 'H2O_3m']
+#             check_faltantes_wc = (faltantes[col_wc] < lim_faltantes).all(axis=1)
+#
+# #-------------------------------------------------------------------------------------------
+# # metodo de los cuadrantes para wc (wCO2 y wq)
+# #-------------------------------------------------------------------------------------------
+# # para el nivel de 3 m
+#             if check_calidad_wc.all() & check_faltantes_wc.all():
+#
+#                 totales_wc_cuad = flujo_wc_cuad(flujos_desvios)
+#                 totales_wc_cuad.to_csv (ruta_salida_cuad_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#                 S_wc_cuad = flujo_wc_S(totales_wc_cuad,flujos_medios)
+#                 S_wc_cuad.to_csv (ruta_salida_S_wc + datos + str(day).zfill(2) + '_' + str(arch).zfill(4) + '.csv', index=False)
+#
+#             else:
+#                 continue
